9cfca48514ca987d7c38a6d55a142d52597ca3d5
jphandrigan <jphandrigan@gmail.com>
2015-09-16 22:45:21 -0400

    Made a bunch of changes to social top bar...
    needs more styling.

--- parts/nav-top-infobar.php
+++ parts/nav-top-infobar.php
@@ -1,6 +1,28 @@
-<div class="show-for-medium-up contain-to-grid">
-	<nav class="social-bar" data-topbar>
-		<section class="top-bar-section">
-				</section>
-	</nav>
+<div class="show-for-large-up">
+	<div class="social-bar">
+		<div class="row">
+			<div class="large-8 columns">
+ 				 <a href="/volsites/add-volunteer-opportunity" class="button">Post Opportunity</a>
+	  	</div>
+	  	<div class="large-4 columns">
+	  		<div class="icon-bar five-up">
+     									<a href="<?php echo get_theme_mod('facebook_setting','http://www.facebook.com');?>" class="item">
+					            	<i class="fa fa-facebook"></i>
+					            </a>
+					            <a href="<?php echo get_theme_mod('twitter_setting','http://www.twitter.com');?>" class="item">
+					            	<i class="fa fa-twitter"></i>
+					            </a>
+					            <a href="<?php echo get_theme_mod('pinterest_setting','http://www.pinterest.com');?>" class="item">
+					            	<i class="fa fa-pinterest"></i>
+					            </a>
+					           <a href="<?php echo get_theme_mod('google_setting','http://www.google.com');?>" class="item">
+					            	<i class="fa fa-google-plus"></i>
+					            </a>
+					           <a href="<?php echo 'mailto:' . get_theme_mod('email_setting','info@volunteerhalifax.ca');?>" class="item">
+					            	<i class="fa fa-envelope"></i>
+					            </a>
+     							</div>
+	  	</div>
+		</div>
+	</div>
 </div>
