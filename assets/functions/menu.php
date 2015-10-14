f6c2dd69a2d5877d1c9c12c422b08d124cee4760
John Paul Handrigan <info@digitalconcoction.net>
2015-10-13 21:49:01 -0400

    adding organization and volunteer menus

--- assets/functions/menu.php
+++ assets/functions/menu.php
@@ -3,7 +3,9 @@
 register_nav_menus(
 	array(
 		'main-nav' => __( 'The Main Menu', 'jointstheme' ),   // Main nav in header
-		'footer-links' => __( 'Footer Links', 'jointstheme' ) // Secondary nav in footer
+		'footer-links' => __( 'Footer Links', 'jointstheme' ), // Secondary nav in footer
+		'org-nav' => __( 'Organization Menu', 'jointstheme'), //The organization menu
+		'vol-nav' => __( 'Volunteer Menu', 'jointstheme') // The volunteer menu
 	)
 );
 
@@ -24,7 +26,40 @@ function joints_top_nav() {
         'walker' => new Top_Bar_Walker(),
     ));
 } /* End Top Menu */
-
+// Organization Menu
+function joints_org_nav() {
+	 wp_nav_menu(array(
+        'container' => false,                           // Remove nav container
+        'container_class' => '',                        // Class of container
+        'menu' => 'The Org Menu', 'jointstheme',                                   // Menu name
+        'menu_class' => 'top-bar-menu right',            // Adding custom nav class
+        'theme_location' => 'org-nav',                // Where it's located in the theme
+        'before' => '',                                 // Before each link <a>
+        'after' => '',                                  // After each link </a>
+        'link_before' => '',                            // Before each link text
+        'link_after' => '',                             // After each link text
+        'depth' => 5,                                   // Limit the depth of the nav
+        'fallback_cb' => false,                         // Fallback function (see below)
+        'walker' => new Top_Bar_Walker(),
+    ));
+} /* End Org Menu */
+// Volunteer Menu
+function joints_vol_nav() {
+	 wp_nav_menu(array(
+        'container' => false,                           // Remove nav container
+        'container_class' => '',                        // Class of container
+        'menu' => 'The Org Menu', 'jointstheme',                                   // Menu name
+        'menu_class' => 'top-bar-menu right',            // Adding custom nav class
+        'theme_location' => 'vol-nav',                // Where it's located in the theme
+        'before' => '',                                 // Before each link <a>
+        'after' => '',                                  // After each link </a>
+        'link_before' => '',                            // Before each link text
+        'link_after' => '',                             // After each link text
+        'depth' => 5,                                   // Limit the depth of the nav
+        'fallback_cb' => false,                         // Fallback function (see below)
+        'walker' => new Top_Bar_Walker(),
+    ));
+} /* End Volunteer Menu */
 function joints_off_canvas() {
 	    wp_nav_menu(array(
 	        'container' => false,                           // Remove nav container
